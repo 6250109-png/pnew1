9ccb6bd2e6c14cf688abc20cd94a9bc27f272e49
6250109-png <6250109@gmail.com>
2026-01-25 17:02:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
 const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'p1.yybbtech.de5.net';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzE1MjZjNzg5Mjc3N2QwMDQzMTNhYmIyODIyMTM2YTIiLCJ0IjoiZjQ2NGRkMzMtZWYzYy00MzVmLWFmZTUtNDEzZmNmY2Q0ZmE4IiwicyI6Ik1ESTRNREUyTjJRdE9UaGhZaTAwWlRSakxXRmlaVEV0Wm1RNE16TmtNV1kxT1dSaCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzE1MjZjNzg5Mjc3N2QwMDQzMTNhYmIyODIyMTM2YTIiLCJ0IjoiODExYmRkY2YtNDAyMS00ZGUwLWJhMmYtZTFkZTlkY2VjNDFmIiwicyI6Ik16Um1ORFpqTURBdE5XWmtaaTAwTkRsaExXRmlPVFl0TURJNFpEQmtNRE13TlRnNCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
 const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
@@ -625,4 +625,5 @@ app.get("/", async function(req, res) {
 });
 
 app.listen(PORT, () => console.log(`http server is running on port:${PORT}!`));
+
 
